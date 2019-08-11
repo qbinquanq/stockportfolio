f630c8f2edb16887753414134704717cf128ef7f
Bin Quan Wang (Vincent) <binquanwang1991@gmail.com>
2019-08-11 14:49:13 -0400

    Created new Project using Spring boot

---
+++ src/main/java/com/microforce/StockPortfolioApplication.java
@@ -0,0 +1,25 @@
+package com.microforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication(scanBasePackages = {"com.microforce.controller"})
+public class StockPortfolioApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioApplication.class, args);
+	}
+	//@Override public void configureViewResolvers(ViewResolverRegistry registry) {
+		  // TODO Auto-generated method stub
+		  //WebMvcConfigurer.super.configureViewResolvers(registry);
+			//registry.jsp("/WEB-INF/html/", ".html"); 
+			  
+		  //InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		  //resolver.setPrefix("/WEB-INF/html/"); 
+		  //resolver.setSuffix(".html");
+		 // registry.viewResolver(resolver); 
+    //}
+}
